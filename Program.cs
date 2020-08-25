83d3cfc6de3098997016aa39670220f4e1fbb142
alkaitagi <alkaitagi@outlook.com>
2020-08-25 19:51:14 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INNO_F20_CC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test_1.Launch();
+        }
+    }
+}
---
+++ Tests/Test_1.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace INNO_F20_CC
+{
+    static class Test_1
+    {
+        public static void Launch() =>
+            Console.Write("Launched");
+    }
+}
