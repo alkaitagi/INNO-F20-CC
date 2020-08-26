f0a7ab532b523bc6ad9e0cdd3db27f6288d0a810
alkaitagi <alkaitagi@outlook.com>
2020-08-26 22:00:41 +0300

    Read filename from args

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     class Program
     {
-        static void Main(string[] args) =>
-            TokenAnalyzer.Analyze("source.txt");
+        static void Main(string[] args) => 
+            TokenAnalyzer.Analyze(args[0]);
     }
 }
